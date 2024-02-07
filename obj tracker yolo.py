2c7b6b34af7dabac4de2cec290322ca70d102fba
om1007 <117250624+om1007@users.noreply.github.com>
2024-02-07 22:28:38 +0530

    Update obj tracker yolo.py

--- obj tracker yolo.py
+++ obj tracker yolo.py
@@ -1,42 +1,31 @@
-
-# CONTOURS DETECTION 
-
-import os
 import cv2
+import ultralytics
+from ultralytics import YOLO
 
-img = cv2.imread(r"C:\Users\om\Desktop\CV\i.png")
+# load yolov8 model 
 
-img_gray = cv2.cvtColor (img, cv2.COLOR_BGR2GRAY)
+model = YOLO(r'C:\Users\om\Desktop\CV\yolov8n.pt')
 
+# load video 
 
-# Contours detect the ite spaces in the img 
-ret, thresh = cv2.threshold(img_gray,127,255,cv2.THRESH_BINARY_INV)
+cap = cv2.VideoCapture(r"C:\Users\om\Desktop\CV\test.mp4")
 
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+ret = True
+#read frames 
+while ret:
+    ret, frame = cap.read()
 
-# Count the number of contours
-num_contours = len(contours)
-
-#finding all outlines in img > 200 & draving rect around them 
-for cnt in contours:
-    print(cv2.contourArea(cnt))
-
-    if cv2.contourArea(cnt) > 1800: 
-      # dravs outline on all objects present in  original image 
-    #  cv2.drawContours(img, cnt,-1,(0,255,0))
+    # detect objects & track objects
     
-        x1,y1 , w,h = cv2.boundingRect(cnt) 
-
-    cv2.rectangle(img, (x1,y1), (x1 + w, y1 + h), (0,255,0),2) 
-
-# Display the number of contours using cv2.putText
-cv2.putText(img, f"Contours: {num_contours - 1}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)
-while True:
-    cv2.imshow("img", img)
+    results = model.track(frame, persist=True)
     
-    # cv2.imshow("img_gray", img_gray)
-    # cv2.imshow("thresh", thresh)
-    if cv2.waitKey(1) == ord('q'):  # Check for 'q' press every 1ms
-        break
 
-cv2.destroyAllWindows()
+    # plot results
+
+    frame_ = results[0].plot()
+
+
+    # visualize 
+    cv2.imshow('frame',frame_)
+    if cv2.waitKey(25) & 0XFF == ord('q'):
+            break
